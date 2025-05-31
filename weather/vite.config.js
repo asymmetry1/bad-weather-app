91d7f9a73089de8dce4b15591b17825a5f51fb0d
asymmetry1 <kuronamiku@gmail.com>
2025-06-01 00:43:33 +0700

    Proxy Setting

--- weather/vite.config.js
+++ weather/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  const config = {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: process.env.VITE_BASE_URL,
+          changeOrigin: true,
+        },
+      },
+    },
+  };
+  return defineConfig(config);
+};
